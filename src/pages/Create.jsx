d3d1dbc766b05d354458066bee60426995ee3030
sidx04 <siddpal04@gmail.com>
2025-03-11 00:00:35 +0530

    user url handling

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -15,11 +15,10 @@ const Create = () => {
     checkMint: false,
     checkUpdate: false,
     publicKey: "",
-    twitterURL: "",
-    telegramURL: "",
-    websiteURL: "",
+    twitterUrl: "",
+    telegramUrl: "",
+    websiteUrl: "",
   });
-
 
   const handleChange = (e) => {
     const { name, value, type, checked } = e.target;
@@ -114,13 +113,18 @@ const Create = () => {
     <div className="pt-28 pb-20 bg-black text-white relative font-['Product Sans']">
       <form
         onSubmit={handleSubmit}
-        className="flex flex-col rounded-[30px] bg-[#23232333] border border-[#343434] text-white p-8 w-auto xl:mx-auto max-w-6xl mx-3 relative">
-        <h1 className="text-3xl md:text-5xl font-semibold text-center uppercase">Create Solana Token</h1>
-        <p className="text-md md:text-xl text-gray-400 text-center mt-4">Easily Create your own Solana SPL Token in just 7+1 steps without Coding.</p>
+        className="flex flex-col rounded-[30px] bg-[#23232333] border border-[#343434] text-white p-8 w-auto xl:mx-auto max-w-6xl mx-3 relative"
+      >
+        <h1 className="text-3xl md:text-5xl font-semibold text-center uppercase">
+          Create Solana Token
+        </h1>
+        <p className="text-md md:text-xl text-gray-400 text-center mt-4">
+          Easily Create your own Solana SPL Token in just 7+1 steps without
+          Coding.
+        </p>
 
-        <div className='items-center flex-col flex justify-between'>
+        <div className="items-center flex-col flex justify-between">
           <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-6">
-
             <div className="grid grid-cols-1 gap-6 mt-6">
               <label htmlFor="imageUpload" className="cursor-pointer">
                 <div className="bg-[#232323] w-full p-6 md:p-10 rounded-[30px] mt-6 text-center">
@@ -131,7 +135,11 @@ const Create = () => {
                       className="mx-auto rounded-lg w-40 h-40 object-cover"
                     />
                   ) : (
-                    <img src="./imageupload.svg" alt="Upload" className="mx-auto" />
+                    <img
+                      src="./imageupload.svg"
+                      alt="Upload"
+                      className="mx-auto"
+                    />
                   )}
                   <p className="text-3xl font-semibold mt-2">Upload Image</p>
                   <p className="text-[#7D7D7D] text-xl mt-1">
@@ -148,43 +156,47 @@ const Create = () => {
               </label>
 
               <div className="bg-[#232323] w-full p-6 md:p-10 rounded-[30px] mt-6 text-left space-y-8">
-                <p className="text-2xl font-semibold mb-4">Add Link for Token Metadata</p>
+                <p className="text-2xl font-semibold mb-4">
+                  Add Link for Token Metadata
+                </p>
 
                 <div className="flex flex-col">
                   <label className="text-lg">Twitter or X Link</label>
-                  <input 
-                    type="text" 
-                    placeholder="Twitter URL" 
-                    name="twitterURL" 
-                    value={formData.twitterURL} 
+                  <input
+                    type="text"
+                    placeholder="Twitter URL"
+                    name="twitterUrl"
+                    value={formData.twitterUrl}
                     onChange={handleChange}
-                    className="p-3 border border-[#7d7d7d] rounded-md bg-[#232323] mt-2" />
+                    className="p-3 border border-[#7d7d7d] rounded-md bg-[#232323] mt-2"
+                  />
                 </div>
 
                 <div className="flex flex-col">
                   <label className="text-lg">Telegram Link</label>
-                  <input 
-                    type="text" 
-                    placeholder="Telegram URL" 
-                    name="telegramURL"
-                    value={formData.telegramURL}
+                  <input
+                    type="text"
+                    placeholder="Telegram URL"
+                    name="telegramUrl"
+                    value={formData.telegramUrl}
                     onChange={handleChange}
-                    className="p-3 border border-[#7d7d7d] rounded-md bg-[#232323] mt-2" />
+                    className="p-3 border border-[#7d7d7d] rounded-md bg-[#232323] mt-2"
+                  />
                 </div>
 
                 <div className="flex flex-col">
                   <label className="text-lg">Website Link</label>
-                  <input 
-                    type="text" 
-                    placeholder="Website URL" 
-                    name="websiteURL"
-                    value={formData.websiteURL}
+                  <input
+                    type="text"
+                    placeholder="Website URL"
+                    name="websiteUrl"
+                    value={formData.websiteUrl}
                     onChange={handleChange}
-                    className="p-3 border border-[#7d7d7d] rounded-md bg-[#232323] mt-2" />
+                    className="p-3 border border-[#7d7d7d] rounded-md bg-[#232323] mt-2"
+                  />
                 </div>
               </div>
             </div>
-
 
             <div className="grid grid-cols-1 gap-6 mt-6">
               <div className="bg-[#232323] w-full p-6 md:p-10 rounded-[30px] mt-6 space-y-4">
@@ -233,7 +245,9 @@ const Create = () => {
                   />
                 </div>
                 <div className="flex flex-col">
-                  <label className="text-lg">Description <span className='text-red-600'>*</span></label>
+                  <label className="text-lg">
+                    Description <span className="text-red-600">*</span>
+                  </label>
                   <input
                     type="text"
                     name="description"
@@ -245,38 +259,51 @@ const Create = () => {
                 </div>
 
                 <div className="flex flex-col">
-                  <div className="text-[#83f8ff] font-semibold">Modify Creator Information <span className='text-[#7d7d7d]'>(optional)</span></div>
-                  <div className='pt-2 pb-3 space-y-1'>
-
-                      <div className="flex flex-col">
-                        <label className="text-lg">Creator Name</label>
-                        <input type="text" placeholder="Creator Name" className="p-3 border border-[#7d7d7d] rounded-md bg-[#232323] mt-2" />
-                      </div>
-                      <div className="flex flex-col">
-                        <label className="text-lg">Creator Website</label>
-                        <input type="text" placeholder="Creator Website" className="p-3 border border-[#7d7d7d] rounded-md bg-[#232323] mt-2" />
-                      </div>
+                  <div className="text-[#83f8ff] font-semibold">
+                    Modify Creator Information{" "}
+                    <span className="text-[#7d7d7d]">(optional)</span>
                   </div>
-                    
+                  <div className="pt-2 pb-3 space-y-1">
+                    <div className="flex flex-col">
+                      <label className="text-lg">Creator Name</label>
+                      <input
+                        type="text"
+                        placeholder="Creator Name"
+                        className="p-3 border border-[#7d7d7d] rounded-md bg-[#232323] mt-2"
+                      />
+                    </div>
+                    <div className="flex flex-col">
+                      <label className="text-lg">Creator Website</label>
+                      <input
+                        type="text"
+                        placeholder="Creator Website"
+                        className="p-3 border border-[#7d7d7d] rounded-md bg-[#232323] mt-2"
+                      />
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-     
+          </div>
+        </div>
 
-        <p className='text-[#83f8ff] text-left text-2xl mt-10 mb-5'>
+        <p className="text-[#83f8ff] text-left text-2xl mt-10 mb-5">
           Professional Options
         </p>
-        <p className='text-xl mb-1'>Authority Control</p>
-        <p className='text-[#7d7d7d] text-md mb-4'>
-          Solana Token has 3 authorities: Freeze Authority, Mint Authority, and Update Authority.<br/> 
+        <p className="text-xl mb-1">Authority Control</p>
+        <p className="text-[#7d7d7d] text-md mb-4">
+          Solana Token has 3 authorities: Freeze Authority, Mint Authority, and
+          Update Authority.
+          <br />
           Revoking them to attracts more people to invest in the token.
         </p>
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-6 mt-6'>
-          <div className='leading-[20px] inline-block p-3 bg-[#232323] rounded-xl overflow-hidden space-y-6'>
-            <p className='text-[#83f8ff]'>Revoke Mint</p>
-            <p className='text-[#7d7d7d]'>Mint Authority allows the token developer to mint more supply of your token.</p>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-6">
+          <div className="leading-[20px] inline-block p-3 bg-[#232323] rounded-xl overflow-hidden space-y-6">
+            <p className="text-[#83f8ff]">Revoke Mint</p>
+            <p className="text-[#7d7d7d]">
+              Mint Authority allows the token developer to mint more supply of
+              your token.
+            </p>
             <div className="justify-between flex items-center">
               <p className="font-semibold">+0.05 SOL</p>
               <input
@@ -289,37 +316,42 @@ const Create = () => {
             </div>
           </div>
 
-          <div className='leading-[20px] inline-block p-3 bg-[#232323] rounded-xl overflow-hidden space-y-6'>
-            <p className='text-[#83f8ff]'>Revoke Freeze</p>
-            <p className='text-[#7d7d7d]'>Freeze Authority allows token developer to freeze token accounts of holders.</p>
-            <div className='justify-between items-center flex'>
-                <p className="font-semibold">+0.05 SOL</p>
-                <input
-                  type="checkbox"
-                  name="checkFreeze"
-                  checked={formData.checkFreeze}
-                  onChange={handleChange}
-                  className="mr-2"
-                />{" "}
+          <div className="leading-[20px] inline-block p-3 bg-[#232323] rounded-xl overflow-hidden space-y-6">
+            <p className="text-[#83f8ff]">Revoke Freeze</p>
+            <p className="text-[#7d7d7d]">
+              Freeze Authority allows token developer to freeze token accounts
+              of holders.
+            </p>
+            <div className="justify-between items-center flex">
+              <p className="font-semibold">+0.05 SOL</p>
+              <input
+                type="checkbox"
+                name="checkFreeze"
+                checked={formData.checkFreeze}
+                onChange={handleChange}
+                className="mr-2"
+              />{" "}
             </div>
           </div>
-        
-          <div className='leading-[20px] inline-block p-3 bg-[#232323] rounded-xl overflow-hidden space-y-6'>
-          <p className='text-[#83f8ff]'>Revoke Update</p>
-          <p className='text-[#7d7d7d]'>Update Authority allows token developer to update the token metadata.</p>
-            <div className='justify-between items-center flex'>
-                <p className="font-semibold">+0.05 SOL</p>
-                <input
-                  type="checkbox"
-                  name="checkUpdate"
-                  checked={formData.checkUpdate}
-                  onChange={handleChange}
-                  className="mr-2"
-                />{" "}
+
+          <div className="leading-[20px] inline-block p-3 bg-[#232323] rounded-xl overflow-hidden space-y-6">
+            <p className="text-[#83f8ff]">Revoke Update</p>
+            <p className="text-[#7d7d7d]">
+              Update Authority allows token developer to update the token
+              metadata.
+            </p>
+            <div className="justify-between items-center flex">
+              <p className="font-semibold">+0.05 SOL</p>
+              <input
+                type="checkbox"
+                name="checkUpdate"
+                checked={formData.checkUpdate}
+                onChange={handleChange}
+                className="mr-2"
+              />{" "}
             </div>
           </div>
         </div>
-
 
         <button
           type="submit"
